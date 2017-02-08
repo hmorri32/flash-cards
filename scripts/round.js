4eaaf1931a35d2c185628f4a87679b9a6f1cf812
Hugh Morrison <hmorri32@gmail.com>
2017-02-08 14:31:17 -0700

    write feedback tests for round function

--- scripts/round.js
+++ scripts/round.js
@@ -1,13 +1,21 @@
+import Guess from "../scripts/guess";
 
 
 
 function Round(deck) {
-  this.deck = deck
-  this.guesses = []
+  this.deck = deck;
+  this.guesses = [];
+  this.guessCount = 0;
 }
 
 Round.prototype.currentCard = function(){
-  return 'suh'
+  return this.deck.cards.shift()
+}
+
+Round.prototype.recordGuess = function(userGuess) {
+  let guess = new Guess({response: userGuess, question: this.currentCard()})
+  this.guesses.push(guess)
+  this.guessCount ++;
 }
 
 export default Round
--- tests/round-test.js
+++ tests/round-test.js
@@ -5,23 +5,117 @@ import Guess              from "../scripts/guess";
 import Deck               from "../scripts/deck";
 
 
-
-
-
 describe('testing round', function(){
   it("round should be a function", function() {
     assert.isFunction(Round);
   });
 
-  it('should do the stuff', function(){
+  it('should accept a card and a deck ', function(){
     let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
-    let card2 = new Card({question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000"})
-    let deck  = new Deck([card1, card2])
+    let deck  = new Deck([card1])
     deck.addCard(card1)
-    deck.addCard(card2)
 
     let round = new Round(deck)
 
-    console.log(round.currentCard())
+    expect(deck.cards).to.deep.equal([{ question: 'What is the capital of Alaska?', answer: 'Juneau' } ])
   })
+
+  it('should be able to keep track of guesses by using an array ', function() {
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    deck.addCard(card1)
+
+    let round = new Round(deck)
+
+    assert.isArray(round.guesses)
+  })
+
+  it('should have a currentCard function', function() {
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    assert.isFunction(round.currentCard);
+  })
+
+  it('currentCard should return the current card', function() {
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    deck.addCard(card1)
+
+    expect(round.currentCard()).to.deep.equal({question: "What is the capital of Alaska?", answer: "Juneau"})
+  })
+
+  it('should recognize recordGuess as a function', function() {
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    assert.isFunction(round.recordGuess);
+  })
+
+  it('should record and keep count of guesses', function() {
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    deck.addCard(card1)
+    round.recordGuess('juneau')
+
+    expect(round.guesses.length).to.equal(1)
+  })
+
+  it('should be able to determine whether a guess is incorrect', function(){
+
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    deck.addCard(card1)
+    round.recordGuess('suuuuhhhhhhhh')
+
+
+    expect(round.guesses[0].correct()).to.be.false
+
+  })
+
+  it('should be able to determine whether a guess is correct', function(){
+
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    deck.addCard(card1)
+    round.recordGuess('Juneau')
+
+    expect(round.guesses[0].correct()).to.be.true
+
+  })
+
+  it('should be able to give negative feedback for incorect guesses', function(){
+
+    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let deck  = new Deck([card1])
+    let round = new Round(deck)
+    deck.addCard(card1)
+    round.recordGuess('suuuuhhhhhhhh')
+
+
+    expect(round.guesses[0].correct()).to.be.false
+
+  })
+
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
