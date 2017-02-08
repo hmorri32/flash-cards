1a449417edc17c60197aa2da554f581c91612349
Hugh Morrison <hmorri32@gmail.com>
2017-02-08 16:18:41 -0700

    write file path functions

--- scripts/cards-generator.js
+++ scripts/cards-generator.js
@@ -1,8 +1,20 @@
-// import Card from "../scripts/flash";
-//
-//
-// function CardGenerator(card){
-//   this.card = card
-// }
-//
-// export default CardGenerator
+import Card from "../scripts/flash";
+import fs   from 'fs';
+
+
+function CardGenerator({filePath}){
+  this.cards = this.splitter(filePath)
+}
+
+CardGenerator.prototype.splitter = function(filePath) {
+  let words = fs.readFileSync(`./${filePath}`, 'utf8').split('\n');
+  return words.map ( card => {
+    let cardSplit = card.split(",");
+    let textQuestion = cardSplit[0];
+    let textAnswer = cardSplit[1];
+    return new Card({ question: textQuestion, answer: textAnswer})
+  })
+}
+
+
+export default CardGenerator
--- tests/cards-generator-test.js
+++ tests/cards-generator-test.js
@@ -1,21 +1,14 @@
 import { expect, assert } from 'chai';
 import CardGenerator      from '../scripts/cards-generator'
 import Card               from '../scripts/flash';
-import fs                 from 'fs';
 
-const words = fs.readFileSync('./cards.txt', 'utf8').split('\n');
-
-const splitter = text => {
-  return words.map ( card => {
-    let cardSplit = card.split(",");
-    let textQuestion = cardSplit[0];
-    let textAnswer = cardSplit[1];
-    return new Card({ question: textQuestion, answer: textAnswer})
-  })
-}
+// console.log(cardgen)
 
 describe('testing card-generator', function(){
+
   it.only('should work', function(){
-    console.log(splitter())
+    let cardgen = new CardGenerator({filePath: 'cards.txt'})
+    console.log(cardgen)
+
   })
 })
