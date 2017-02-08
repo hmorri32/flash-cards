b4f4576ede76657b7af8ab06e754f4048eda0c79
Hugh Morrison <hmorri32@gmail.com>
2017-02-08 16:34:52 -0700

    refactor guess and guesstest using es6

--- scripts/guess.js
+++ scripts/guess.js
@@ -1,24 +1,23 @@
-function Guess({ response, question }){
-  this.response = response;
-  this.question = question;
-}
-
-Guess.prototype.correct = function(){
-  let { answer } = this.question;
-   if(answer === this.response) {
-     return true
-   } else {
-     return false
-   }
-}
-
-Guess.prototype.feedback = function(){
-  if(this.correct()){
-    return "Correct!"
-  } else {
-    return "Incorrect!!!"
+class Guess {
+  constructor({ response, question }){
+    this.response = response;
+    this.question = question;
+  }
+  correct(){
+    let { answer } = this.question;
+     if(answer === this.response) {
+       return true
+     } else {
+       return false
+     }
+  }
+  feedback(){
+    if(this.correct()){
+      return "Correct!"
+    } else {
+      return "Incorrect!!!"
+    }
   }
 }
-
 
 export default Guess;
--- tests/guess-test.js
+++ tests/guess-test.js
@@ -3,31 +3,31 @@ import Guess              from "../scripts/guess";
 import Card               from "../scripts/flash";
 
 
-describe("testing guess", function() {
-  it('guess should be a function', function() {
+describe("testing guess", () => {
+  it('guess should be a function', () => {
 
     assert.isFunction(Guess)
   });
 
-  it('should recognize card as a function', function() {
-    
+  it('should recognize card as a function', () => {
+
     assert.isFunction(Card)
   })
 
-  it('function guess should be able to accept a response', function() {
+  it('function guess should be able to accept a response', () => {
     let guess = new Guess ({ response: 'suhhhhhh'});
 
     assert.equal(guess.response, 'suhhhhhh')
   })
 
-  it('Guess should be able to accept a card object', function() {
+  it('Guess should be able to accept a card object', () => {
     let card  = new Card({});
     let guess = new Guess({ question: card });
 
     assert.isObject(guess.question);
   });
 
-  it("Guess should be able to access both the card's question and answer", function(){
+  it("Guess should be able to access both the card's question and answer", () => {
     let card  = new Card ({ question: "Which planet is closest to the sun?", answer: "mercury"})
     let guess = new Guess ({question: card})
 
@@ -35,28 +35,28 @@ describe("testing guess", function() {
     assert.equal(guess.question.question, "Which planet is closest to the sun?")
   })
 
-  it('guess should accept a wrong answer and determine that it is wrong ', function(){
+  it('guess should accept a wrong answer and determine that it is wrong ', () => {
     let card  = new Card ({ question: "Which planet is closest to the sun?", answer: "mercury"})
     let guess = new Guess ({ response: 'suh', question: card})
 
     assert.equal(guess.correct(), false);
   })
 
-  it('guess should accept a correct answer and determine that it is correct ', function(){
+  it('guess should accept a correct answer and determine that it is correct ', () => {
     let card  = new Card ({ question: "Which planet is closest to the sun?", answer: "mercury"})
     let guess = new Guess ({ response: "mercury", question: card})
 
     assert.equal(guess.correct(), true);
   })
 
-  it("should give negative feedback if you got the incorrect answer", function(){
+  it("should give negative feedback if you got the incorrect answer", () => {
     let card  = new Card ({ question: "Which planet is closest to the sun?", answer: "mercury"})
     let guess = new Guess ({ response: "suhhhhhhh", question: card})
 
     assert.equal(guess.feedback(), "Incorrect!!!");
   })
 
-  it("should give positive feedback if you got the correct answer", function(){
+  it("should give positive feedback if you got the correct answer", () => {
     let card  = new Card ({ question: "Which planet is closest to the sun?", answer: "mercury"})
     let guess = new Guess ({ response: "mercury", question: card})
 
