2222ab071b7858f280b13fe014632e992da6c458
Hugh Morrison <hmorri32@gmail.com>
2017-02-08 16:37:04 -0700

    expand upon cardgenerator test

--- tests/cards-generator-test.js
+++ tests/cards-generator-test.js
@@ -2,11 +2,9 @@ import { expect, assert } from 'chai';
 import CardGenerator      from '../scripts/cards-generator'
 import Card               from '../scripts/flash';
 
-// console.log(cardgen)
-
 describe('testing card-generator', () => {
 
-  it('should work', () => {
+  it('should display card object from text file', () => {
     let cardgen = new CardGenerator({filePath: 'cards.txt'})
     console.log(cardgen)
 
